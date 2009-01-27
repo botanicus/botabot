45d64049fcb5a1d2313ecc890ec51703a5dbf522
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-01-27 01:48:11 +0100

    Actually last commit isn't such bad solution, it just need some refactoring

--- lib/botabot/plugin.rb
+++ lib/botabot/plugin.rb
@@ -3,6 +3,22 @@ require "botabot/role"
 require "botabot/roles"
 
 module BotaBot
+  # TODO: merge PluginInstance and Plugin classes
+  class PluginInstance
+    attr_reader :bot, :muc, :msg, :nick
+    def initialize(block, bot, muc, msg)
+      self.register(block)
+      @bot  = bot
+      @muc  = muc
+      @msg  = msg
+      @nick = msg.from
+    end
+
+    def register(block)
+      self.class.send(:define_method, :run, &block)
+    end
+  end
+
   class Plugin
     attr_writer :roles
     attr_reader :name, :desc, :rights
@@ -21,26 +37,9 @@ module BotaBot
       Roles[nick].rights >= self.rights
     end
 
-    def klass
-      Class.new do
-        attr_reader :bot, :muc, :msg, :nick
-        def initialize(block, bot, muc, msg)
-          self.register(block)
-          @bot  = bot
-          @muc  = muc
-          @msg  = msg
-          @nick = msg.from
-        end
-
-        def register(block)
-          self.class.send(:define_method, :run, &block)
-        end
-      end
-    end
-    
     def run(muc, message, *args)
       BotaBot.logger.debug("Called plugin #{@name} with args #{args.inspect}")
-      klass.new(@block, @client, muc, message).run(*args) if self.has_rights?(message.from)
+      PluginInstance.new(@block, @client, muc, message).run(*args) if self.has_rights?(message.from)
     rescue
       BotaBot.logger.error("Exception: #$!")
       BotaBot.logger.error("Details: #{$!.backtrace.join("\n")}")

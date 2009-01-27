0a319ebaf156d1e00dd8cde70c4eaf51c7107465
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2009-01-27 12:14:44 +0100

    Ctrl-c 1x => reload (plugins and core), 2x => terminate

--- lib/botabot.rb
+++ lib/botabot.rb
@@ -1,3 +1,4 @@
+require "find"
 require "botabot/bot"
 require "botabot/config"
 require "botabot/kernel"
@@ -25,16 +26,35 @@ module BotaBot
   end
   
   def self.init(profile)
+    self.register_signals
     bot.init(config.jid, config.password)
     bot.join(config.muc(profile))
     Plugins.load
     return bot
   end
+
+  def self.register_signals
+    trap("INT") do
+      if @interrupted_at.nil? || (Time.now - @interrupted_at).to_i > 2
+        self.reload
+      else
+        exit
+      end
+      @interrupted_at = Time.now
+    end
+  end
   
   def self.reload
-    Dir["#{BotaBot.root}/lib/botabot/*.rb"].each do |library|
-      load(library)
+    Find.find("#{BotaBot.root}/lib") do |file|
+      begin
+        load(file) if file.match(/\.rb$/)
+      rescue
+        BotaBot.logger.error("Failed to load file #{file}: #{$!}")
+        next
+      end
     end
+    BotaBot.logger.debug("Reloaded library")
+    Plugins.load
   end
   
   def self.run(profile)
